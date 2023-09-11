238d3734c06b86bcce15d0c9f7fab687bd78f75b
hoorasadat <mirsaanei@gmail.com>
2023-09-11 04:50:25 +0000

    add controllers

---
+++ AssetTrackingSystem.API/Controllers/DepartmentsController.cs
@@ -0,0 +1,46 @@
+﻿using AssetTrackingSystem.API.Interfaces;
+using AssetTrackingSystem.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssetTrackingSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        private readonly IDepartmentRepository _departmentRepository;
+
+        public DepartmentsController(IDepartmentRepository departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+        }
+
+
+
+        // GET: api/<DepartmentsController>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Route("getalldepartments")]
+        public async Task<ActionResult<IList<Department>>> Get()
+        {
+            try
+            {
+                return Ok(await _departmentRepository.GetAllDepartments());
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving from the database!");
+            }
+        }
+
+
+
+        // GET api/<DepartmentsController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+    }
+}
---
+++ AssetTrackingSystem.API/Controllers/EmployeesController.cs
@@ -0,0 +1,46 @@
+﻿using AssetTrackingSystem.API.Interfaces;
+using AssetTrackingSystem.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssetTrackingSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeesController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+
+
+        // GET: api/<EmployeesController>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Route("getallemployees")]
+        public async Task<ActionResult<IList<Employee>>> Get()
+        {
+            try
+            {
+                return Ok(await _employeeRepository.GetAllEmployees());
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving from the database!");
+            }
+        }
+
+
+
+        // GET api/<EmployeesController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+    }
+}
